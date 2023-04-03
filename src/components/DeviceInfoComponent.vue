15260fd8086fc40e03ccf9a583068165c8f88716
irishkooky <yoshihiro.kurosaki.15@gmail.com>
2023-04-03 13:03:10 +0900

    🐛 表示されないバグ修正

--- src/components/DeviceInfoComponent.vue
+++ src/components/DeviceInfoComponent.vue
@@ -2,71 +2,71 @@
 import { ref, withDefaults } from "vue";
 const interfaceBlock = ref(false);
 const routingBlock = ref(false);
-const srpolicyBlock =ref(false);
+const srpolicyBlock = ref(false);
 
-export interface Props {
-  deviceInfo:
-    | {
-      // general infomation
-        asNumber: string
-        rd: string
-        importPolicy?: string
-        exportPolicy?: string
-        // interface
-        interface: {
-          name: string
-          description: string
-          ipAddress: {
-            address: string
-            isSecondary: boolean
-          }[]
-        }[]
-        // routing
-        bgpNeighbor: {
-          address: string
-          as: string
-        }[]
-        // srPolicy
-        srPolicy?: {
-          endpoint: string
-          color:string
-        }[]
-      }
-    | null
+interface Props {
+  deviceInfo: {
+    // general infomation
+    asNumber: string;
+    rd: string;
+    importPolicy?: string;
+    exportPolicy?: string;
+    // interface
+    interface: {
+      name: string;
+      description: string;
+      ipAddress: {
+        address: string;
+        isSecondary: boolean;
+      }[];
+    }[];
+    // routing
+    bgpNeighbor: {
+      address: string;
+      as: string;
+    }[];
+    // srPolicy
+    srPolicy?: {
+      endpoint: string;
+      color: string;
+    }[];
+  } | null;
 }
 
-const props = withDefaults(defineProps<Props>(),{
-  asNumber: '65534',
-  rd: '65534:100',
-  exportPolicy: 'SET-C10',
-  interface: [
-    {
-      name: 'bundleEther1.100',
-      description: 'to-Node11',
-      ipAddress: [{ address: '192.168.21.1/24', isSecondary: false }],
-    },
-    {
-      name: 'gigabitEthernet0/0/0/0',
-      description: 'to - Node21',
-      ipAddress: [
-        { address: '192.168.21.1/24', isSecondary: false },
-        { address: '192.168.22.1/29', isSecondary: true },
-        { address: '192.168.23.1/29', isSecondary: true },
-      ],
-    },
-  ],
-  // routing
-  bgpNeighbor: [
-    { address: '192.168.21.2', as: '18000' },
-    { address: '192.168.21.3', as: '18000' },
-  ],
-  // 値（srPolicy）が無ければ非表示
-  srPolicy: [
-    { endpoint: 'ACR11', color: '10' },
-    { endpoint: 'ACR12', color: '10' },
-    { endpoint: 'WSE12', color: '10' }
+withDefaults(defineProps<Props>(), {
+  deviceInfo: {
+    asNumber: "65534",
+    rd: "65534:100",
+    exportPolicy: "SET-C10",
+    interface: [
+      {
+        name: "bundleEther1.100",
+        description: "to-Node11",
+        ipAddress: [{ address: "192.168.21.1/24", isSecondary: false }],
+      },
+      {
+        name: "gigabitEthernet0/0/0/0",
+        description: "to - Node21",
+        ipAddress: [
+          { address: "192.168.21.1/24", isSecondary: false },
+          { address: "192.168.22.1/29", isSecondary: true },
+          { address: "192.168.23.1/29", isSecondary: true },
+        ],
+      },
     ],
-})
+    // routing
+    bgpNeighbor: [
+      { address: "192.168.21.2", as: "18000" },
+      { address: "192.168.21.3", as: "18000" },
+    ],
+    // 値（srPolicy）が無ければ非表示
+    srPolicy: [
+      { endpoint: "ACR11", color: "10" },
+      { endpoint: "ACR12", color: "10" },
+      { endpoint: "WSE12", color: "10" },
+    ],
+  },
+});
 
 // withDefaults(
 //     defineProps<{
@@ -103,14 +103,10 @@ const props = withDefaults(defineProps<Props>(),{
 //       deviceInfo: null,
 //     }
 //   )
-
-  </script>
+</script>
 
 <template>
-  <v-card
-    class="mx-auto"
-    style="width: 500px"
-  >
+  <v-card class="mx-auto" style="width: 500px">
     <!-- General Infomation start -->
     <v-card-title> General Infomation </v-card-title>
 
@@ -130,14 +126,18 @@ const props = withDefaults(defineProps<Props>(),{
 
         <tr>
           <td>Import Policy</td>
-          <td v-if="deviceInfo?.importPolicy">{{ deviceInfo?.importPolicy }}</td>
+          <td v-if="deviceInfo?.importPolicy">
+            {{ deviceInfo?.importPolicy }}
+          </td>
           <td v-else>-</td>
         </tr>
         <v-divider style="border-top-width: 0px"></v-divider>
 
         <tr>
           <td>Export Policy</td>
-          <td v-if="deviceInfo?.exportPolicy">{{ deviceInfo?.exportPolicy }}</td>
+          <td v-if="deviceInfo?.exportPolicy">
+            {{ deviceInfo?.exportPolicy }}
+          </td>
           <td v-else>-</td>
         </tr>
       </tbody>
@@ -158,7 +158,10 @@ const props = withDefaults(defineProps<Props>(),{
 
     <v-card elevation="0" style="padding-left: 16px; padding-right: 16px">
       <div v-show="interfaceBlock">
-        <div v-for="(inter, interIndex) in deviceInfo?.interface" :key="interIndex">
+        <div
+          v-for="(inter, interIndex) in deviceInfo?.interface"
+          :key="interIndex"
+        >
           <v-card-title> {{ inter.name }} </v-card-title>
 
           <v-table fixed-header style="padding-left: 16px; padding-right: 16px">
@@ -171,7 +174,10 @@ const props = withDefaults(defineProps<Props>(),{
               <tr>
                 <td>IP Address</td>
                 <td>
-                  <div v-for="(ipAddress, ipAddressIndex) in inter.ipAddress" :key="ipAddressIndex">
+                  <div
+                    v-for="(ipAddress, ipAddressIndex) in inter.ipAddress"
+                    :key="ipAddressIndex"
+                  >
                     {{ ipAddress.address }}
                     <span v-if="ipAddress.isSecondary"> secondary </span>
                   </div>
@@ -202,11 +208,14 @@ const props = withDefaults(defineProps<Props>(),{
         <v-card-title> BGP Neighbor </v-card-title>
         <v-table fixed-header style="padding-left: 16px; padding-right: 16px">
           <tbody>
-            <tr  v-for="(routing, routingIndex) in deviceInfo?.bgpNeighbor" :key="routingIndex">
+            <tr
+              v-for="(routing, routingIndex) in deviceInfo?.bgpNeighbor"
+              :key="routingIndex"
+            >
               <td>{{ routing.address }}</td>
-              <td> AS {{ routing.as }}</td>
+              <td>AS {{ routing.as }}</td>
             </tr>
-              <v-divider style="border-top-width: 0px"></v-divider>
+            <v-divider style="border-top-width: 0px"></v-divider>
           </tbody>
         </v-table>
       </div>
@@ -215,15 +224,15 @@ const props = withDefaults(defineProps<Props>(),{
 
     <!-- SR Policy start -->
     <div v-if="deviceInfo?.srPolicy">
-      <v-card-actions style="padding: 0px;">
+      <v-card-actions style="padding: 0px">
         <v-card-title> SR Policy </v-card-title>
-          <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
 
-          <v-btn
+        <v-btn
           :icon="srpolicyBlock ? 'mdi-chevron-up' : 'mdi-chevron-down'"
           @click="srpolicyBlock = !srpolicyBlock"
-          >
-          </v-btn>
+        >
+        </v-btn>
       </v-card-actions>
 
       <div v-show="srpolicyBlock">
@@ -231,20 +240,19 @@ const props = withDefaults(defineProps<Props>(),{
           <v-table fixed-header style="padding-left: 16px; padding-right: 16px">
             <thead>
               <tr>
-                <th class="text-left">
-                  SR
-                </th>
-                <th class="text-left">
-                  Color
-                </th>
+                <th class="text-left">SR</th>
+                <th class="text-left">Color</th>
               </tr>
             </thead>
             <tbody>
-              <tr  v-for="(srpolicy, srpolicyIndex) in deviceInfo?.srPolicy" :key="srpolicyIndex">
+              <tr
+                v-for="(srpolicy, srpolicyIndex) in deviceInfo?.srPolicy"
+                :key="srpolicyIndex"
+              >
                 <td>{{ srpolicy.endpoint }}</td>
                 <td>{{ srpolicy.color }}</td>
               </tr>
-                <v-divider style="border-top-width: 0px"></v-divider>
+              <v-divider style="border-top-width: 0px"></v-divider>
             </tbody>
           </v-table>
         </div>
